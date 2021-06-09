dfad968d224ee2b656e7ed5dd1b12930c5558a2f
MugeraH <bwosihughes24@gmail.com>
2021-06-10 01:04:38 +0300

    edit userserializer remove is_admin field

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username','email','first_name','last_name','password','is_admin']
+        fields = ['id','username','email','first_name','last_name','password']
         extra_kwargs = {
             'password':{'write_only':True,}
         }
