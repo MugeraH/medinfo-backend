dd0d0fa7439db87053c854a5ce5f8bc7304877e6
MugeraH <bwosihughes24@gmail.com>
2021-06-09 04:10:55 +0300

    add drug and illness views

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from .views import IllnessListView,IllnessDetailView,IllnessSearchDetailView,DrugListView,DrugDetailView,DrugSearchDetailView
+
+
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
 
+path('illness',IllnessListView.as_view(),name="illness-list"),  
+path('illness/<int:pk>',IllnessDetailView.as_view(),name="illness-detail"),  
+path('illness/<str:search_term>',IllnessSearchDetailView.as_view(),name="searched-illness"), 
+
+path('drug',DrugListView.as_view(),name="drug-list"),  
+path('drug/<int:pk>',DrugDetailView.as_view(),name="drug-detail"),  
+path('drug/<str:search_term>',DrugSearchDetailView.as_view(),name="searched-drug"), 
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,93 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+# third party imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import status
+
+from rest_framework.authentication import SessionAuthentication,BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+from .models import Illness,Drug,Post,Reply
+from .serializers import PostSerializer,IllnessSerializer,DrugSerializer,ReplySerializer
+
+
+class IllnessListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    serializer_class = IllnessSerializer
+    queryset = Illness.objects.all()
+    
+    def get(self,request,*args,**kwargs): 
+        return self.list(self,request,*args,**kwargs)
+    
+    def post(self,request,*args,**kwargs): 
+        return self.create(request,*args,**kwargs)
+    
+    
+class IllnessDetailView(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    serializer_class = IllnessSerializer
+    queryset = Illness.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class IllnessSearchDetailView(APIView):
+    def get(self,request,search_term):
+        illnesses = Illness.search_business_by_search_term(search_term)
+        serializer = IllnessSerializer(illnesses, many=True)
+        return Response(serializer.data)
+    
+    
+    
+    
+class DrugListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    serializer_class = DrugSerializer
+    queryset = Drug.objects.all()
+    
+    def get(self,request,*args,**kwargs): 
+        return self.list(self,request,*args,**kwargs)
+    
+    def post(self,request,*args,**kwargs): 
+        return self.create(request,*args,**kwargs)
+    
+    
+class DrugDetailView(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    serializer_class = DrugSerializer
+    queryset = Drug.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class DrugSearchDetailView(APIView):
+    def get(self,request,search_term):
+        drugs = Drug.search_business_by_search_term(search_term)
+        serializer = DrugSerializer(drugs, many=True)
+        return Response(serializer.data)
+    
