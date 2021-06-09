1033ea9a92f6953d45ab0c60692ea0de65f768b6
MugeraH <bwosihughes24@gmail.com>
2021-06-09 04:22:04 +0300

    add urls for reply and post

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IllnessListView,IllnessDetailView,IllnessSearchDetailView,DrugListView,DrugDetailView,DrugSearchDetailView
+from .views import IllnessListView,IllnessDetailView,IllnessSearchDetailView,DrugListView,DrugDetailView,DrugSearchDetailView,PostReplyDetailView,PostListView,PostDetailView,ReplyListView,ReplyDetailView
 
 
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -13,6 +13,14 @@ path('illness/<str:search_term>',IllnessSearchDetailView.as_view(),name="searche
 path('drug',DrugListView.as_view(),name="drug-list"),  
 path('drug/<int:pk>',DrugDetailView.as_view(),name="drug-detail"),  
 path('drug/<str:search_term>',DrugSearchDetailView.as_view(),name="searched-drug"), 
+
+path('post',PostListView.as_view(),name="post-list"),  
+path('post/<int:pk>',PostDetailView.as_view(),name="post-detail"), 
+
+path('reply',ReplyListView.as_view(),name="reply-list"),  
+path('reply/<int:pk>',ReplyDetailView.as_view(),name="reply-detail"), 
+path('post/reply/<int:pk>',PostReplyDetailView.as_view(),name="post-reply-detail"), 
+
 
 ]
 
